907b9948f582b65775f2485567d0a033836574f3
Marcin Tomaszewski <marcintomaszewski1993@gmail.com>
2026-08-11 14:14:24 +0200

    Fix: register typeLabels and typeIcons for personnel-file to appear in UI dropdown

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ Hooks.once("init", () => {
 
   // Register Data Model for the Custom Journal Entry Page
   CONFIG.JournalEntryPage.dataModels["personnel-file"] = PersonnelFileDataModel;
+  CONFIG.JournalEntryPage.typeLabels["personnel-file"] = "COC-CASE-FILES.PageType";
+  CONFIG.JournalEntryPage.typeIcons["personnel-file"] = "fas fa-user-secret";
 
   // Register the Sheet for the Custom Journal Entry Page
   DocumentSheetConfig.registerSheet(JournalEntryPage, "coc-case-files", PersonnelFileSheet, {
